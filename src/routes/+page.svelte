a6b2c25027c8469092358cc54e48452e03ae900f
RyanL-AtBDGL <rlo@blackdiamondgroup.com>
2023-07-03 11:22:50 -0600

    Updated branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream
 <html lang="en">
     <body>
         <input type="password"/>
         <button id="submitButton">Submit<button/>
     </body>
 </html>
+=======
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+>>>>>>> Stashed changes
